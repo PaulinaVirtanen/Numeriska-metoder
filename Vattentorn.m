bcc1b76b11171e3ff251f958e1eea9c7d7346de7
PaulinaVirtanen <pvir@ug.kth.se>
2025-02-12 14:59:26 +0100

    Add files via upload

--- Vattentorn.m
+++ Vattentorn.m
@@ -19,20 +19,20 @@ function T = trapets(func, n, interval)
     a = interval(1);  
     b = interval(2);  
     h = (b - a) / n;  
-    x = linspace(a, b, n+1);       
-    y = func(x);      
+    x = linspace(a, b, n+1);      
+    y = func(x);     
     
     T = h * (sum(y) - (y(1) + y(end)) / 2);  % Trapetsformeln
 end
 
-f = @(x,beta) (exp(beta * x) + 8) / (1 + (x / 5).^3);
+f = @(x,beta) (exp(beta .* x) + 8) ./ (1 + (x ./ 5).^3);
 f02 = @(x) f(x,0.2);
 
 % Trapetsregeln
-V_trapets = pi * trapets(f02, 100, [0 20]);
+V_trapets = pi * trapets(f02, 20, [0 20]);
 
 % Simpsons metod
-V_simpsons = pi * simpsons(f02, 100, [0 20]);
+V_simpsons = pi * simpsons(f02, 20, [0 20]);
 
 disp(['Resultat av trapetsregeln: ', num2str(V_trapets)])
 disp(['Resultat av Simpsons metod: ', num2str(V_simpsons)])
